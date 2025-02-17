83b79e94bb023193843a9865f276bdd41bfa934e
Danny79k <belloli.danny@gmail.com>
2025-02-17 21:35:20 +0000

    eventos añadidos

--- src/layouts/layout.jsx
+++ src/layouts/layout.jsx
@@ -3,7 +3,10 @@ import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
 
+
+
 export default function Layout() {
+
 
     return (
         <>
--- src/pages/Eventos.jsx
+++ src/pages/Eventos.jsx
@@ -1,10 +1,38 @@
+import useFetch from "../components/useFetch"
+import Loading from "../components/Loading"
 
+export default function Eventos() {
 
-export default function Eventos(){
+    const { data, loading, error } = useFetch("https://jeffrey.informaticamajada.es/api/events")
 
-    return(
+    if (loading) return (<><Loading /></>)
+    if (error) return (<>ERROR</>)
+    const eventos = data.data
+    console.log(data);
+
+    return (
         <div className="mt-20">
-            <h1>Eventos</h1>
+            <div className="grid grid-cols-3 gap-2">
+                {eventos.map(evento => {
+                    return (
+                        <div className="max-w-sm rounded-2xl shadow-lg overflow-hidden border border-gray-200 dark:border-gray-700" key={evento.id}>
+                            <img className="w-full h-48 object-cover" src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeJQeJyzgAzTEVqXiGe90RGBFhfp_4RcJJMQ&s"} alt={"name"} />
+                            <div className="p-5">
+                                <h2 className="text-2xl font-bold ">{evento.title}</h2>
+                                <p className="mt-2 ">{evento.description}</p>
+                                <div className="mt-4 border-t pt-4 dark:border-gray-600">
+                                    <p className="text-sm text-gray-500 dark:text-gray-400">
+                                        <span>tipo event:</span>{evento.access_type}
+                                    </p>
+                                    <p className="text-sm text-gray-500 dark:text-gray-400">
+                                        <span>fecha evento: </span>{evento.date_start.slice(0,10)}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
