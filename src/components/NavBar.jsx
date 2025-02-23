21c5da03ea0bd332b762710e60200375dde4f895
Danny79k <belloli.danny@gmail.com>
2025-02-23 23:25:35 +0000

    feat: añadido sign in

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -116,14 +116,12 @@ export default function NavBar() {
             <div className={`transition-all duration-300 ${isOpenNav ? "block" : "hidden"} ${navBg} fixed right-100 top-20 z-20`}>
                 <div className={`grid grid-cols-4  p-4 space-y-4 ${navBg}`}>
                     <NavLink to={"https://jeffrey.informaticamajada.es/login"} className=" hover:text-yellow-5000 col-end-1" onClick={() => setIsOpen(false)}>Login</NavLink>
-                    <NavLink to={"#"} className=" hover:text-yellow-500 col-end-1" onClick={() => setIsOpen(false)}>Sign in</NavLink>
+                    <NavLink to={"https://jeffrey.informaticamajada.es/register"} className=" hover:text-yellow-500 col-end-1" onClick={() => setIsOpen(false)}>Sign in</NavLink>
                     <NavLink to={"#"} className=" hover:text-yellow-500 col-end-1" onClick={() => setIsOpen(false)}>Mis Asociaciones</NavLink>
                     <NavLink to={"#"} className=" hover:text-yellow-500 col-end-1" onClick={() => setIsOpen(false)}>Mis Eventos</NavLink>
                     <NavLink to={"#"} className=" hover:text-yellow-500 col-end-1" onClick={() => setIsOpen(false)}>Mi Calendario</NavLink>
+                    <NavLink id="logout" className="bg-red-500 col-end-1 p-1 hover:border-none rounded-2xl">Logout</NavLink>
                 </div>
-                    <form method="get" className="bg-red-500" action="https://jeffrey.informaticamajada.es/logout-user">
-                        <button type="submit" className="bg-red-500 p-1 hover:border-none rounded-2xl">Logout</button>
-                    </form>
             </div>
         </div>
     );
